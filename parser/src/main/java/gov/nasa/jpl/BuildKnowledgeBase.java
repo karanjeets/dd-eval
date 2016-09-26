3e1142df35e584637492683209559255e7b20034
Karanjeet Singh <connect.karanjeet@gmail.com>
2016-09-26 01:28:15 -0700

    Added support for kb-model

--- parser/src/main/java/gov/nasa/jpl/BuildKnowledgeBase.java
+++ parser/src/main/java/gov/nasa/jpl/BuildKnowledgeBase.java
@@ -10,16 +10,19 @@ import java.io.*;
  */
 public class BuildKnowledgeBase {
 
-    public static void processUrlFile(File file, String nbModelDirPath, String cosineModelDirPath) throws Exception {
+    public static void processUrlFile(File file, String nbModelDirPath, String cosineModelDirPath, String knowledgeModelDirPath) throws Exception {
         File nbModel = new File(nbModelDirPath + File.separator + file.getName().replace("input", "nb"));
         File cosineModel = new File(cosineModelDirPath + File.separator + file.getName().replace("input", "cosine"));
+        File kbModel = new File(knowledgeModelDirPath + File.separator + file.getName().replace("input", "kb"));
         BufferedReader br = null;
         BufferedWriter nbBw = null;
         BufferedWriter cosineBw = null;
+        BufferedWriter kbBw = null;
         try {
             br = new BufferedReader(new FileReader(file));
             nbBw = new BufferedWriter(new FileWriter(nbModel));
             cosineBw = new BufferedWriter(new FileWriter(cosineModel));
+            kbBw = new BufferedWriter(new FileWriter(kbModel));
             for (String url; (url = br.readLine()) != null;) {
                 String content = null;
                 try {
@@ -28,6 +31,7 @@ public class BuildKnowledgeBase {
                         content = content.replaceAll("\\n", " ");
                         nbBw.append("1\t" + content + "\n");
                         cosineBw.append(content + "\n");
+                        kbBw.append(url.trim() + "|_|" + content + "\n");
                         System.out.println("Processed - File: " + file.getAbsolutePath() + " URL: " + url);
                     } else {
                         System.out.println("No Content Found - File: " + file.getAbsolutePath() + " URL: " + url);
@@ -41,12 +45,13 @@ public class BuildKnowledgeBase {
             IOUtils.closeQuietly(br);
             IOUtils.closeQuietly(nbBw);
             IOUtils.closeQuietly(cosineBw);
+            IOUtils.closeQuietly(kbBw);
         }
     }
 
-    public static void processUrlFile(File[] files, String nbModelDirPath, String cosineModelDirPath) throws Exception {
+    public static void processUrlFile(File[] files, String nbModelDirPath, String cosineModelDirPath, String knowledgeModelDirPath) throws Exception {
         for (File file: files) {
-            processUrlFile(file, nbModelDirPath, cosineModelDirPath);
+            processUrlFile(file, nbModelDirPath, cosineModelDirPath, knowledgeModelDirPath);
         }
     }
 
@@ -62,15 +67,17 @@ public class BuildKnowledgeBase {
         String outDirPath = args[1];
         String nbModelDirPath = outDirPath + File.separator + "nb-models";
         String cosineModelDirPath = outDirPath + File.separator + "cosine-models";
+        String knowledgeModelDirPath = outDirPath + File.separator + "kb-models";
 
         File urlFile = new File(urlPath);
         makeSafeDir(nbModelDirPath);
         makeSafeDir(cosineModelDirPath);
+        makeSafeDir(knowledgeModelDirPath);
 
         if (urlFile.isDirectory()) {
-            processUrlFile(urlFile.listFiles(), nbModelDirPath, cosineModelDirPath);
+            processUrlFile(urlFile.listFiles(), nbModelDirPath, cosineModelDirPath, knowledgeModelDirPath);
         } else {
-            processUrlFile(urlFile, nbModelDirPath, cosineModelDirPath);
+            processUrlFile(urlFile, nbModelDirPath, cosineModelDirPath, knowledgeModelDirPath);
         }
     }
 }
